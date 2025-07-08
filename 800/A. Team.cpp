1fcb5dd4b6c5fb05ebf53897076d6e01eeb29977
Angkon Kar <angkon310@gmail.com>
2025-07-09 04:10:20 +0600

    Created A. Team

---
+++ 800/A. Team.cpp
@@ -0,0 +1,36 @@
+// Problem: A. Team
+// Contest: Codeforces Round #143 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/231/A
+// Tag: Brute Force, Greedy
+// Memory Limit: 256 Megabytes
+// Time Limit: 2 Second
+
+#include<iostream>
+using namespace std;
+ 
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield);cout.precision(10);cout.setf(ios::fixed,ios::floatfield);
+ 
+int main()
+{
+    int n, i, j, k = 0, c = 0, a[3];
+    cin>>n;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        k = 0;
+        for (int j = 0; j < 3; j++)
+        {
+            cin>>a[j];
+        }
+        for (int j = 0; j < 3; j++)
+        {
+            if(a[j] == 1) k++;
+        }
+        if(k > 1) c++;
+    }
+    
+    cout << c << endl;
+    
+    return 0;
+}
