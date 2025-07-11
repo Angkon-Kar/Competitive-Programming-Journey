8d6585d4f682c7942b97929ecf9c0aedd748ab54
Angkon Kar <angkon310@gmail.com>
2025-07-11 17:45:49 +0600

    added A. Domino piling files code

---
+++ 800/A_Domino piling.cpp
@@ -0,0 +1,21 @@
+// Problem: A. Domino piling
+// Contest: Codeforces Beta Round 47
+// URL: https://codeforces.com/problemset/problem/50/A
+// Tag: Greedy, Math
+// Memory Limit: 256 Megabytes
+// Time Limit: 2 Second
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int M, N, i, k;
+    cin >> M >> N;
+
+    i = M * N;
+    k = i / 2;
+
+    cout << k << endl;
+    return 0;
+}
