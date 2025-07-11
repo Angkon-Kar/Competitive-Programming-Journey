1054c3d2896b963f2bb01c44248b1f7134e40f72
Angkon Kar <angkon310@gmail.com>
2025-07-11 18:08:49 +0600

    added A. Helpful Maths file code

---
+++ 800/A_Helpful_Maths.cpp
@@ -0,0 +1,41 @@
+// Problem: A. Helpful Maths
+// Contest: Codeforces Round #197 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/339/A
+// Tag: Greedy, Implementation, Sortings, Strings
+// Memory Limit: 256 Megabytes
+// Time Limit: 2 Second
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    string digits = "";
+    // Extract only the digits
+    for (char c : s)
+    {
+        if (c != '+')
+        {
+            digits += c;
+        }
+    }
+
+    // Sort the digits
+    sort(digits.begin(), digits.end());
+
+    // Reconstruct the output with plus signs
+    for (size_t i = 0; i < digits.length(); ++i)
+    {
+        if (i > 0)
+            cout << "+";
+        cout << digits[i];
+    }
+    cout << endl;
+
+    return 0;
+}
