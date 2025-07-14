7603f3c97c72d4e6410d906bcac81c579131e239
Angkon Kar <angkon310@gmail.com>
2025-07-15 00:35:54 +0600

    update some code

--- Codeforces/800/A_Blackboard_Game.cpp
+++ Codeforces/800/A_Blackboard_Game.cpp
@@ -4,19 +4,26 @@
 
 #include <iostream>
 #include <string>
- 
-int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
-    std::cin >> t;
-    while (t--) {
+    cin >> t;
+    while (t--)
+    {
         int n;
-        std::cin >> n;
-        if (n % 4 == 0) {
-            std::cout << "Bob\n";
-        } else {
-            std::cout << "Alice\n";
+        cin >> n;
+        if (n % 4 == 0)
+        {
+            cout << "Bob\n";
+        }
+        else
+        {
+            cout << "Alice\n";
         }
     }
     return 0;
