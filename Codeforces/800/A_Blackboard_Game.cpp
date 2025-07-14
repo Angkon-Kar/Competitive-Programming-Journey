d9fad3ebf65e987c42f71af723bd36dce56454d4
Angkon Kar <angkon310@gmail.com>
2025-07-15 00:33:13 +0600

    Added A. Blackboard Game code

---
+++ Codeforces/800/A_Blackboard_Game.cpp
@@ -0,0 +1,23 @@
+// Problem: A. Blackboard Game
+// URL: https://codeforces.com/contest/2123/problem/A
+// Tag: Math
+
+#include <iostream>
+#include <string>
+ 
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int t;
+    std::cin >> t;
+    while (t--) {
+        int n;
+        std::cin >> n;
+        if (n % 4 == 0) {
+            std::cout << "Bob\n";
+        } else {
+            std::cout << "Alice\n";
+        }
+    }
+    return 0;
+}
