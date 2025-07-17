236ebf783f62b921940835ce1a19ae7066d4ced7
Angkon Kar <angkon310@gmail.com>
2025-07-18 01:58:09 +0600

    Added beecrowd beginner problem 1000.cpp

---
+++ Beginner/1000.cpp
@@ -0,0 +1,13 @@
+// Problem: Hello World!
+// URL: https://judge.beecrowd.com/en/problems/view/1000
+// Tag:
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    
+    return 0;
+}
