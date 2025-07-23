54017e7cc24cf8eb761b435189f40e5b4c065566
Angkon Kar <angkon310@gmail.com>
2025-07-23 21:45:34 +0600

    Added A. Lucky

---
+++ Codeforces/800/A_Plus_or_Minus.cpp
@@ -0,0 +1,27 @@
+// Problem: Added A. Plus or Minus
+// URL: https://codeforces.com/problemset/problem/1676/A
+// Tag: Implementation
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        string s;
+        cin >> s;
+
+        if (s[0] + s[1] + s[2] == s[3] + s[4] + s[5])
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
