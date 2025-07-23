5e3b2fcc1dd385a8ff865f81ac11735f5d0a741b
Angkon Kar <angkon310@gmail.com>
2025-07-23 22:26:07 +0600

    Added Simple Sum file

---
+++ Beecrowd/Beginner/1003.cpp
@@ -0,0 +1,15 @@
+// Problem: Simple Sum
+// URL: https://judge.beecrowd.com/en/problems/view/1003
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << "SOMA = " << a + b << endl;
+
+    return 0;
+}
