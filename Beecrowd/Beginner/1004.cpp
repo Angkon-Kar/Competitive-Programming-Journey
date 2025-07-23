f59c5bf54d6ab6e2de69c7173e733eab551c8e92
Angkon Kar <angkon310@gmail.com>
2025-07-23 22:54:37 +0600

    Simple Product

---
+++ Beecrowd/Beginner/1004.cpp
@@ -0,0 +1,14 @@
+// Problem: Simple Product
+// URL: https://judge.beecrowd.com/en/problems/view/1004
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "PROD = " << a * b << endl;
+
+    return 0;
+}
