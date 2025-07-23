f269034457c37146a63b0f4e3ba856d6fe619921
Angkon Kar <angkon310@gmail.com>
2025-07-23 23:24:00 +0600

    added Average 2 file

---
+++ Beecrowd/Beginner/1006.cpp
@@ -0,0 +1,16 @@
+// Problem: Average 2
+// URL: https://judge.beecrowd.com/en/problems/view/1006
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double a, b,c ;
+    cin >> a >> b>>c ;
+
+    cout << fixed << setprecision(1) << "MEDIA = " << ((a*2) + (b*3) + (c*5)) / 10 << endl;
+    
+    return 0;
+}
