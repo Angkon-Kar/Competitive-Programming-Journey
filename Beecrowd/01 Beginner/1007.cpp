99666c7bdce620f2bcba6cc2175d888878579bf8
Angkon Kar <angkon310@gmail.com>
2025-09-13 22:47:28 +0600

    Added Difference Problems Solve

---
+++ Beecrowd/01 Beginner/1007.cpp
@@ -0,0 +1,18 @@
+// Problem: Difference
+// URL: https://judge.beecrowd.com/en/problems/view/1007
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    int e = (a * b) - (c * d);
+
+    cout << "DIFERENCA = " << e << endl;
+
+    return 0;
+}
