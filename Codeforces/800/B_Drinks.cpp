c7c56581a32b1548cd2cec15f1f8599248808fbf
Angkon Kar <angkon310@gmail.com>
2026-03-22 00:39:44 +0600

    B. Drinks

---
+++ Codeforces/800/B_Drinks.cpp
@@ -0,0 +1,23 @@
+// Problem: B. Drinks
+// URL: https://codeforces.com/problemset/problem/200/B
+// Tag: Implementation, Math
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    double sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        double p;
+        cin >> p;
+        sum += p;
+    }
+
+    cout << fixed << setprecision(12) << sum / n << endl;
+
+    return 0;
+}
