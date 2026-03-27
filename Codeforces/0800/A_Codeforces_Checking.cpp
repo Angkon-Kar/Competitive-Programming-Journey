5c3bd35a50752ced31312570ac41959dd5ff9e0f
Angkon Kar <angkon310@gmail.com>
2026-03-27 21:53:46 +0600

    Correct Code for: A. Codeforces Checking

--- Codeforces/0800/A_Codeforces_Checking.cpp
+++ Codeforces/0800/A_Codeforces_Checking.cpp
@@ -4,22 +4,32 @@
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
+void solve() {
+    char c;
+    cin >> c;
+
+    string target = "codeforces";
+
+    if (target.find(c) != string::npos) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
+
 int main() {
-    
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin >> t;
 
-    while (t--)
-    {
-        string s;
-        cin >> s;
-
-        if (s == "codeforces")
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
+    while (t--) {
+        solve();
     }
 
     return 0;
