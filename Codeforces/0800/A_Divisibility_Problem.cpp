af86da0b8b180d48be0e1c2652448a98bfe43dca
Angkon Kar <angkon310@gmail.com>
2026-03-29 23:33:10 +0600

    added divisibility problem

---
+++ Codeforces/0800/A_Divisibility_Problem.cpp
@@ -0,0 +1,28 @@
+// Problem: A. Divisibility Problem
+// URL: https://codeforces.com/problemset/problem/1328/A
+// Tag: Math
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if (a%b == 0)
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            cout << b - (a%b) << endl;
+        }
+    }
+    
+    return 0;
+}
