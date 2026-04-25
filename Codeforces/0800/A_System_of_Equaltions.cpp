fe17d5692661fe2770d00fd88dbabc45b1c7b467
Angkon Kar <angkon310@gmail.com>
2026-04-26 01:32:10 +0600

    Solved from first equations: a^2 + b = n

---
+++ Codeforces/0800/A_System_of_Equaltions.cpp
@@ -0,0 +1,23 @@
+// Problem: System of Equations
+// URL: https://codeforces.com/problemset/problem/214/A
+// Tag: Math
+
+// Solved from first equations: a^2 + b = n
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int count = 0;
+    for (int a = 0; a*a <= n; a++){
+        int b = n - a*a;
+        if ((b >= 0) && (a + b*b == m)){
+            count++;
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
